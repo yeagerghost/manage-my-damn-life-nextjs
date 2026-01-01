ac5f8b345966fa082242477df06901482f15553e
yeagerghost <yeagerghost@gmail.com>
2026-01-01 14:33:49 -0500

    more space adjustment in code

--- src/components/fullcalendar/CalendarViewWithStateManagement.tsx
+++ src/components/fullcalendar/CalendarViewWithStateManagement.tsx
@@ -581,9 +581,9 @@ export const CalendarViewWithStateManagement = ({ calendarAR }: { calendarAR: nu
                 eventResize={eventResize}
                 firstDay={firstDay}
                 locales={allLocales}
-            	locale={i18n.language}
-	        slotDuration={'00:15:00'}                // 15 minute slots globally
-	        slotLabelInterval={'01:00:00'}           // label every 1 hour
+                locale={i18n.language}
+                slotDuration={'00:15:00'}                // 15 minute slots globally
+                slotLabelInterval={'01:00:00'}           // label every 1 hour
             />
         </>
     )
